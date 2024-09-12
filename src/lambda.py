b32da969c567bfea00f5bf94c8f166b925a718c7
Ale <rvalex36@gmail.com>
2024-09-12 17:26:32 -0400

    init commit

---
+++ src/lambda.py
@@ -0,0 +1,11 @@
+import boto3
+import os
+import json
+
+def lambda_handler(event, context):
+    message = event.get('message', 'No message provided')
+    print(f"Received message: {message}")
+    return {
+        'statusCode': 200,
+        'body': json.dumps(f'Message logged: {message}')
+    }
