6a9944301c96fecfcff75acc7764a6e9e738846c
Ale <rvalex36@gmail.com>
2024-09-12 18:14:05 -0400

    testing actions

--- src/lambda.py
+++ src/lambda.py
@@ -4,6 +4,7 @@ import json
 
 def lambda_handler(event, context):
     message = event.get('message', 'No message provided')
+    print("something")
     print(f"Received message: {message}")
     return {
         'statusCode': 200,
