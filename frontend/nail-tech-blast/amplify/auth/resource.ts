e88293aceeb41411bcc1a048bca985fc31656c08
Ale <rvalex36@gmail.com>
2024-12-19 12:46:58 -0500

    amplify auth

--- frontend/nail-tech-blast/amplify/auth/resource.ts
+++ frontend/nail-tech-blast/amplify/auth/resource.ts
@@ -3,6 +3,7 @@ import { defineAuth } from '@aws-amplify/backend';
 /**
  * Define and configure your auth resource
  * @see https://docs.amplify.aws/gen2/build-a-backend/auth
+ * just for github sake
  */
 export const auth = defineAuth({
   loginWith: {
