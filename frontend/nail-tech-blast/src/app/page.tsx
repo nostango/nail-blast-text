b94dd76b533cc8247e927097a9878b6ab5d7e92c
Ale <rvalex36@gmail.com>
2025-02-04 10:22:46 -0500

    removed authentication for now, that will be a separate branch to test and hopefully implement

--- frontend/nail-tech-blast/src/app/page.tsx
+++ frontend/nail-tech-blast/src/app/page.tsx
@@ -1,21 +1,18 @@
 'use client';
 
 import { GroupMessageFormComponent } from '@/components/group-message-form';
-import { Authenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
 
 export default function Home() {
 
   return (
-    <Authenticator>
       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-        <div className="w-full max-w-md p-4 bg-white rounded shadow-lg">
-          <h1 className="text-2xl font-bold text-center mb-4">Send a Text Blast</h1>
+        <div className="w-full max-w-md p-4 bg-white rounded shadow-lg md:max-w-2xl md:p-8">
+          <h1 className="text-2xl font-bold text-center mb-4 md:text-4xl">Send a Text Blast</h1>
 
           {/* Group Message Form component */}
           <GroupMessageFormComponent />
         </div>
       </div>
-    </Authenticator>
   );
 }
